1e846a48ac215391421f05211ccfcb16c7837a3c
moonch030 <cogus2010@naver.com>
2024-07-10 17:07:41 +0900

    2024-07-10 JdbcTemplate 이름 지정 파라미터1

--- src/main/java/hello/itemservice/repository/jebctemplate/JdbcTemplateItemRepositoryV1.java
+++ src/main/java/hello/itemservice/repository/jebctemplate/JdbcTemplateItemRepositoryV1.java
@@ -4,49 +4,55 @@ import hello.itemservice.domain.Item;
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.util.StringUtils;
 
 import javax.sql.DataSource;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 /**
  *  JdbcTemplate
  */
-public class jdbcTemplateItemRepositoryV1 implements ItemRepository {
-
+@Slf4j
+public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
     private final JdbcTemplate template;
 
-    public jdbcTemplateItemRepositoryV1(DataSource dataSource) {
+    public JdbcTemplateItemRepositoryV1(DataSource dataSource) {
         this.template = new JdbcTemplate(dataSource);
     }
 
     @Override
     public Item save(Item item) {
-        String sql = "insert into item(item_name, price, quantity) values(?,?,?)";
-        KeyHolder keyHolder = new GeneratedKeyHolder();;
-        template.update(connection -> {
-            //자동 증가 키
-            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+        String sql = "insert into item(item_name, price, quantity) values (?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        template.update(conection -> {
+            // 자동 증가 키
+            PreparedStatement ps = conection.prepareStatement(sql, new String[]{"id"});
+
             ps.setString(1, item.getItemName());
             ps.setInt(2, item.getPrice());
             ps.setInt(3, item.getQuantity());
+
             return ps;
-        },keyHolder);
+        }, keyHolder);
 
         long key = keyHolder.getKey().longValue();
         item.setId(key);
-        return null;
+
+        return item;
     }
 
     @Override
     public void update(Long itemId, ItemUpdateDto updateParam) {
-        String sql = "update item set item_name=?, price=?, quantity=? where id=?";
+        String sql = "update item set item_name = ?, price = ?, quantity = ? where id = ?";
         template.update(sql,
                 updateParam.getItemName(),
                 updateParam.getPrice(),
@@ -56,24 +62,13 @@ public class jdbcTemplateItemRepositoryV1 implements ItemRepository {
 
     @Override
     public Optional<Item> findById(Long id) {
-        String sql = "select id, item_name, price, quantity from item where id=?";
-        try{
-            Item item = template.queryForObject(sql, itemRowMapper(),id);
-
+        String sql = "select id, item_name, price, quantity from item where id = ?";
+        try {
+            Item item = template.queryForObject(sql, itemRowMapper(), id);
             return Optional.of(item);
-        }catch (EmptyResultDataAccessException e){
+        } catch (EmptyResultDataAccessException e) {
             return Optional.empty();
         }
-    }
-
-    @Override
-    public List<Item> findAll(ItemSearchCond cond) {
-        String itemName = cond.getItemName();
-        Integer maxPrice = cond.getMaxPrice();
-
-        String sql = "select id, item_name, price, quantity from item";
-
-        return template.query(sql, itemRowMapper());
     }
 
     private RowMapper<Item> itemRowMapper() {
@@ -85,5 +80,37 @@ public class jdbcTemplateItemRepositoryV1 implements ItemRepository {
             item.setQuantity(rs.getInt("quantity"));
             return item;
         });
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        String sql = "select id, item_name, price, quantity from item";
+
+        // 동적 쿼리
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            sql += " where";
+        }
+
+        boolean andFlag = false;
+        List<Object> param = new ArrayList<>();
+        if (StringUtils.hasText(itemName)) {
+            sql += " item_name like concat('%',?,'%')";
+            param.add(itemName);
+            andFlag = true;
+        }
+
+        if (maxPrice != null) {
+            if (andFlag) {
+                sql += " and";
+            }
+            sql += " price <= ?";
+            param.add(maxPrice);
+        }
+
+        log.info("sql={}", sql);
+        return template.query(sql, itemRowMapper(), param.toArray());
     }
 }
