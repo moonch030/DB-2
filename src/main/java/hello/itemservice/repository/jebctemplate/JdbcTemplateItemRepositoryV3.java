471dbcc44fb53965cd05d88e3a30a61f31aaa774
moonch030 <cogus2010@naver.com>
2024-07-10 19:59:25 +0900

    2024-07-10 테스트 - 데이터 롤백

--- src/main/java/hello/itemservice/repository/jebctemplate/JdbcTemplateItemRepositoryV3.java
+++ src/main/java/hello/itemservice/repository/jebctemplate/JdbcTemplateItemRepositoryV3.java
@@ -102,7 +102,7 @@ public class JdbcTemplateItemRepositoryV3 implements ItemRepository {
             if (andFlag) {
                 sql += " and";
             }
-            sql += " price <= ?";
+            sql += " price <= :maxPrice";
         }
 
         log.info("sql={}", sql);
